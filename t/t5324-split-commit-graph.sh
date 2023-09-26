a3f8c293c8d2e6bbfeec0089c28dd60ba9acc625
Jeff King <peff@peff.net>
2023-09-26 02:03:00 -0400

    commit-graph: tighten chain size check
    When we open a commit-graph-chain file, if it's smaller than a single
    entry, we just quietly treat that as ENOENT. That make some sense if the
    file is truly zero bytes, but it means that "commit-graph verify" will
    quietly ignore a file that contains garbage if that garbage happens to
    be short.
    
    Instead, let's only simulate ENOENT when the file is truly empty, and
    otherwise return EINVAL. The normal graph-loading routines don't care,
    but "commit-graph verify" will notice and complain about the difference.
    
    It's not entirely clear to me that the 0-is-ENOENT case actually happens
    in real life, so we could perhaps just eliminate this special-case
    altogether. But this is how we've always behaved, so I'm preserving it
    in the name of backwards compatibility (though again, it really only
    matters for "verify", as the regular routines are happy to load what
    they can).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5324-split-commit-graph.sh
+++ t/t5324-split-commit-graph.sh
@@ -327,6 +327,18 @@ test_expect_success 'verify after commit-graph-chain corruption' '
 	)
 '
 
+test_expect_success 'verify notices too-short chain file' '
+	git clone --no-hardlinks . verify-chain-garbage &&
+	(
+		cd verify-chain-garbage &&
+		git commit-graph verify &&
+		echo "garbage" >$graphdir/commit-graph-chain &&
+		test_must_fail git commit-graph verify 2>test_err &&
+		grep -v "^+" test_err >err &&
+		grep "commit-graph chain file too small" err
+	)
+'
+
 test_expect_success 'verify across alternates' '
 	git clone --no-hardlinks . verify-alt &&
 	(
